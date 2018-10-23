5b492661b25147011db4cc2616ff86111ee81bfc
slim <smedini@gmail.com>
2018-10-23 10:16:43 +0200

    con string

--- src/env.js
+++ src/env.js
@@ -7,10 +7,11 @@ const debug = require('debug')('api:auth')
 
 const ENV = {
     NAME:name,
-    PORT: process.env.PORT || 8080,
+    PORT: process.env.PORT || 80,
     
     REST_PATH: process.env.REST_PATH || "rest",
-    
+
+    DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING,
     DB_NAME: process.env.DB_NAME || "BlueForestTreesDB",
     DB_HOST: process.env.DB_HOST || "localhost",
     DB_PORT: process.env.DB_PORT || 27017,
